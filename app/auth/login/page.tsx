ec37f544d9a0e34458b410d091ff84782b07057e
ErikROCHA <okerik.com@gmail.com>
2024-08-25 21:59:18 -0500

    login auth/login send data to console

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,7 +1,51 @@
+'use client';
+import {useForm} from 'react-hook-form';
+
 export default function Login() {
+  const { register, handleSubmit, formState: {errors} } = useForm();
+
+  const onSubmit = handleSubmit(data => {
+    console.log(data)
+  });
+
   return (
     <div>
-      <span>Login</span>
+      <form onSubmit={onSubmit}>
+        <h1>Login</h1>
+        <label htmlFor="email" className="block">email</label>
+        <input 
+          type="email"
+          { ...register("email", {
+            required: {
+              value: true,
+              message: "Ingresa tu email"
+            }
+          })}
+          className="bg-blue-100"
+        />
+        { errors.email?.message && (
+          <span className="text-red-500 text-xs block mb-2">{errors.email.message as string}</span>
+        )}
+
+        <label htmlFor="password" className="block">Contraseña</label>
+        <input 
+          type="text"
+          { ...register("password", {
+            required: {
+              value: true,
+              message: "Ingresa tu contraseña"
+            }
+          })}
+          className="bg-blue-100"
+        />
+        { errors.password?.message && (
+          <span className="text-red-500 text-xs block mb-2">{errors.password.message as string}</span>
+        )}
+
+        <button className="block bg-blue-500 w-full">
+          Entrar
+        </button>
+      </form>
     </div>
   );
 }
